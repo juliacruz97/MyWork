e176df1d8c926f2db6a3ebe7b5879d203014b7df
juliacruz97 <juliacruz97@hotmail.com>
2024-04-30 20:05:32 -0300

    c4

---
+++ addOne.py
@@ -0,0 +1,7 @@
+#addOne.py 
+#This Prohram reading on a number and prints out one more than that number
+# author: Julia Cruz
+
+number = int (input ('Please enter a number: '))
+newNumber = number + 1
+print (f' {number} plus one is {newNumber}')
