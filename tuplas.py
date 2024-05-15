28e4cd2d725a80a6d48de8a58d14c07e638aef30
juliacruz97 <juliacruz97@hotmail.com>
2024-05-15 15:05:09 -0300

    15_05_24

---
+++ tuplas.py
@@ -0,0 +1,23 @@
+#Understanding how tuples work
+# lunch is my tuple
+# Tuples are immutable
+
+lunch = ("Hamburguer","Suco","Pizza","Pudim")
+#print(lunch)
+#print(lunch [2])
+#print(lunch[-1])
+#print(lunch[0:2])
+#print(lunch[1:]);
+#print(len(lunch))
+#lunch [1] = soda  (Tuples are immutable)
+
+for comida in lunch:
+    print(f' Eu vou comer {comida} ')
+    
+for cont in range (0, len(lunch)):
+    print('Por favor eu quero',lunch [cont])
+    
+for pos,comida in enumerate (lunch):
+    print (f'Eu vou comer {comida} na posicao {pos}')
+    
+print ('Comi pra caramba! ')
