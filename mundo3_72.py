d237ddd0efc32559fd975589a8f3a0ea9ab2adb4
juliacruz97 <juliacruz97@hotmail.com>
2024-05-16 10:36:55 -0300

    16_05

---
+++ mundo3_72.py
@@ -0,0 +1,14 @@
+#Create a program that has a fully populated tuple
+# counting in words, Zora ate twenty.
+
+#The program must read a number from the keyboard (0
+# between 0 and 20) and shows it in full.
+
+import random
+
+cont = int(input("Digite um numero de 0 a 20 "))
+cont = (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+randint = cont
+print(cont)
+
+
--- tuplas.py
+++ tuplas.py
@@ -11,15 +11,33 @@ lunch = ("Hamburguer","Suco","Pizza","Pudim")
 #print(len(lunch))
 #lunch [1] = soda  (Tuples are immutable)
 
-for comida in lunch:
-    print(f' Eu vou comer {comida} ')
+#for comida in lunch:
+    #print(f' Eu vou comer {comida} ')
     
-for cont in range (0, len(lunch)):
-    print('Por favor eu quero',lunch [cont])
+#for cont in range (0, len(lunch)):
+    #print('Por favor eu quero',lunch [cont])
     
-for pos,comida in enumerate (lunch):
-    print (f'Eu vou comer {comida} na posicao {pos}')
+#for pos,comida in enumerate (lunch):
+    #print (f'Eu vou comer {comida} na posicao {pos}')
     
-print ('Comi pra caramba! ')
+#print ('Comi pra caramba! ')
 
-print (sorted (lunch))
+#print (sorted (lunch))
+
+a= (2,5,4)
+b= (5,8,1,2)
+c= b + a
+print(a)
+print(b)
+print(c)
+print(len(c))
+print(c.count(5))
+print(c.index(8))
+print(c.index(2))
+print(c.index(5,1))
+
+pessoa = ("Julia", "44" , "Fem", "99.9")
+#del(pessoa)
+del(pessoa (1)) # dont work
+print(pessoa)
+
